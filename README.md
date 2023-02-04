f16118d87e604e8ebb0ec291164d74a007d22721
Drew Conner <drew@blinkingboxes.net>
2023-02-04 15:09:47 -0600

    some initial minor changes and additions

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # flappy
 Flappy Dragon
+
+Credits to [Hands-on Rust](https://hands-on-rust.com/) by [Herbert Wolverson](https://github.com/thebracket)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use bracket_lib::prelude::*;
+
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        // ctx (context) provides functions for interacting with the
+        // game display; cls() clears the window
+        ctx.cls();
+        ctx.print(1, 1, "Hello, Bracket Terminal!");
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
